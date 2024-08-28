24d002567b40a6247160ff8e1d6ec2b91fd2350e
Thangga7 <beencules@gmail.com>
2024-08-29 00:41:27 +0700

    api

--- src/Pages/API/api.jsx
+++ src/Pages/API/api.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './api.scss';
 const Api = () => {
-    return <div className="container">
-     
-    </div>;
+    return (
+        <div className="container">
+            <a href="https://kkphim.com/help/help.html" className="api_link">
+                https://kkphim.com/help/help.html
+            </a>
+        </div>
+    );
 };
 
 export default Api;
