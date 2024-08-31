485d657e4b782df531570b1afac57861d0586ed6
Thangga7 <beencules@gmail.com>
2024-08-31 15:32:08 +0700

    chore: Add react-multi-carousel package to improve carousel functionality

--- src/Pages/Watch/watch.jsx
+++ src/Pages/Watch/watch.jsx
@@ -8,17 +8,24 @@ function Watch() {
     const [active, setActive] = useState(0);
     const [film, setFilm] = useState('');
     const [activeFilm, setActiveFilm] = useState('');
+    const [content, setContent] = useState([]);
     const { slug } = useParams();
     useEffect(() => {
-        fetch(`https://phimapi.com/phim/${slug}`)
-            .then((res) => res.json())
-            .then((watch) => {
-                setChap(watch.movie);
+        const WathFilm = async () => {
+            try {
+                const res = await fetch(`https://phimapi.com/phim/${slug}`);
+                const watch = await res.json();
+                setContent(watch.movie);
+                console.log(watch);
+
+                setChap(watch.episodes[0].server_data.length);
                 setFilm(watch.episodes[0].server_data[0].link_m3u8);
                 setActiveFilm(watch.episodes[0].server_data);
-
                 document.title = watch.movie.name;
-            });
+            } catch (error) {}
+        };
+
+        WathFilm();
     }, [slug]);
 
     useEffect(() => {
@@ -44,7 +51,7 @@ function Watch() {
         <div className="watch">
             <div className="container">
                 <div className="film__title">
-                    <h1 className="film__name">{`${chap.name} ~ Tập ${active + 1}`}</h1>
+                    <h1 className="film__name">{`${content.name} ~ Tập ${active + 1}`}</h1>
                 </div>
                 <div className="row">
                     <div className="col-lg-9 col-12">
@@ -55,11 +62,11 @@ function Watch() {
                             <div className="list__chap--info">
                                 <p className="list__chap--title"></p>
                                 <p className="list__chap--desc">
-                                    Chọn tập từ 1 đến <span className="chap__desc"> {chap.episode_total}</span>
+                                    Chọn tập từ 1 đến <span className="chap__desc"> {chap}</span>
                                 </p>
                             </div>
                             <ul className="list" id="list">
-                                {Array.from({ length: chap.episode_total }).map((_, i) => (
+                                {Array.from({ length: chap }).map((_, i) => (
                                     <li
                                         style={{ backgroundColor: active === i ? 'green' : '#22252a' }}
                                         className="film__list--chap"
@@ -83,11 +90,11 @@ function Watch() {
                 <div className="film__info">
                     <div className="film__info--desc">
                         <p className="info__title">
-                            Thời gian: <span className="film__time">{chap.time}</span>
+                            Thời gian: <span className="film__time">{content.time}</span>
                         </p>
                         <p className="info__title">
                             Nội dung:
-                            <span className="film__desc">{chap.content}</span>
+                            <span className="film__desc">{content.content}</span>
                         </p>
                     </div>
                 </div>
